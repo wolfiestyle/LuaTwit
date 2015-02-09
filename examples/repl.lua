a596e235836325bc484c44f8a8930b85fa503386
darkstalker <slayerbeast@gmail.com>
2015-02-09 04:09:28 -0300

    update examples/repl.lua

--- examples/repl.lua
+++ examples/repl.lua
@@ -1,81 +1,87 @@
 #!/usr/bin/lua -i
 --
 -- Interactive Lua session with a Twitter client logged in.
--- It uses pl.pretty to display the returned data.
+-- It uses pl.pretty and custom __tostring methods to display the returned data.
 --
 package.path = "../src/?.lua;" .. package.path
 pl = require "pl.import_into" ()
+pretty = pl.pretty
 twitter = require "luatwit"
-util = require "luatwit.util"
 objects = require "luatwit.objects"
-
--- used to display raw json data
-local table_inspect_mt = {}
-table_inspect_mt.__index = table_inspect_mt
-
-function table_inspect_mt:__tostring()
-    return pl.pretty.write(self)
-end
-
-function table_inspect_mt:save(filename)
-    filename = filename or "tw.out"
-    pl.file.write(filename, tostring(self) .. "\n")
-    return self
-end
-
--- get a function name from the resource URL
-function get_resource_by_url(url)
-    for name, decl in pairs(twitter.resources) do
-        if decl[2] == url then
-            return setmetatable({ name = name, decl = decl }, table_inspect_mt)
-        end
-    end
-    return nil
-end
 
 -- initialize the twitter client
 oauth_params = twitter.load_keys("oauth_app_keys", "local_auth")
 client = twitter.api.new(oauth_params)
 
--- used to display tweets
+-- pretty print for resource items
+client.resources._resource_base.__tostring = pl.pretty.write
+
+-- pretty print for tweets
 function client.objects.tweet:__tostring()
     return "<" .. self.user.screen_name .. "> " .. self.text
 end
 
--- used to display DMs
+-- pretty print for DMs
 function client.objects.dm:__tostring()
     return "<" .. self.sender.screen_name .. "> (to: " .. self.recipient.screen_name .. ") " .. self.text
 end
 
--- used to display user profiles
-local user_tmpl = pl.text.Template(
+-- pretty print for user profiles
+local user_tmpl =
 [[Name: @$screen_name ($name)
 Bio: $description
 Location: $location
 Followers: $followers_count, Following: $friends_count, Listed: $listed_count
 Tweets: $statuses_count
----
-]])
-
+---]]
 function client.objects.user:__tostring()
-    return user_tmpl:safe_substitute(self)
+    return (user_tmpl:gsub("$([%w_]+)", self))
 end
 
 -- used to display object lists
 local function list_tostring(self)
-    return table.concat(pl.tablex.pairmap(function(k, v) return k .. ": " .. tostring(v) end, self), "\n") .. "\n"
+    local res = {}
+    for i, item in ipairs(self) do
+        res[#res + 1] = i .. ": " .. tostring(item)
+    end
+    return table.concat(res, "\n") .. "\n"
+end
+
+-- writes the contents of a table to a file
+function table_save(tbl, filename)
+    filename = filename or "tw.out"
+    pretty.dump(tbl, filename)
+    return tbl
 end
 
 -- add default __tostring methods to all objects
 for name, _ in pairs(objects) do
     local obj = client.objects[name]
     if not obj.__tostring then
-        obj.__tostring = name:match("_list$") and list_tostring or table_inspect_mt.__tostring
+        obj.__tostring = name:match("_list$") and list_tostring or pretty.write
     end
-    obj.save = obj.save or table_inspect_mt.save
+    obj.save = obj.save or table_save
 end
 
 -- avoid headers spam
 client.objects.headers.__tostring = nil
 
+-- lists the keys of a table and the type of its values
+function keys(obj)
+    if type(obj) ~= "table" then
+        print(tostring(obj) .. " = " .. type(obj))
+        return
+    end
+    for k, v in pairs(obj) do
+        print(tostring(k) .. " = " .. type(v))
+    end
+end
 
+-- finds a resource item with the specified api path
+function get_resource(path)
+    for name, decl in pairs(client.resources) do
+        if decl[2] == path then
+            return decl
+        end
+    end
+end
