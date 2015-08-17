1729c46c10f8d6ce6179a91fef7717f760369b49
darkstalker <slayerbeast@gmail.com>
2015-08-17 02:59:52 -0300

    added chunked upload support to the tweet.lua example

--- examples/tweet.lua
+++ examples/tweet.lua
@@ -12,22 +12,66 @@ local util = require "luatwit.util"
 local args = lapp [[
 Sends a tweet.
     -m,--media (default "")     Image file to be included with the tweet
+    -c,--chunked (default "")   Upload file as chunked with specified mimetype
     <text...>  (string)         Tweet text
 ]]
 
 local msg = table.concat(args.text, " ")
-local img_file = args.media ~= "" and args.media or nil
+
+local assert = _VERSION >= "Lua 5.3" and assert or function(val, ...)
+    if val then return val, ... end
+    error(tostring(select(1, ...)))
+end
 
 -- initialize the twitter client
 local oauth_params = util.load_keys(cfg.app_keys, cfg.user_keys)
 local client = twitter.api.new(oauth_params)
 
+-- upload the file
+local media
+if args.media ~= "" then
+    if args.chunked ~= "" then
+        -- chunked upload
+        print("reading file: " .. args.media)
+        local file = io.open(args.media, "rb")
+        local chunks, total = {}, 0
+        while true do
+            local buf = file:read(524288)
+            if buf == nil then break end
+            total = total + #buf
+            chunks[#chunks + 1] = buf
+        end
+        file:close()
+        print("upload INIT")
+        local handle = assert(client:upload_media_chunked{
+            command = "INIT",
+            media_type = args.chunked,
+            total_bytes = total,
+        })
+        pretty.dump(handle)
+        for i, data in ipairs(chunks) do
+            print(("upload APPEND %d/%d"):format(i, #chunks))
+            assert(client:upload_media_chunked{
+                command = "APPEND",
+                media_id = handle.media_id_string,
+                segment_index = i-1,
+                media = { filename = "media", data = data },
+            })
+        end
+        print("upload FINALIZE")
+        media = assert(client:upload_media_chunked{ command = "FINALIZE", media_id = handle.media_id_string })
+    else
+        -- regular upload
+        print("uploading file: " .. args.media)
+        local img = assert(util.attach_file(args.media))
+        media = assert(client:upload_media{ media = img })
+        media._request = nil  -- don't print binary data to the tty
+    end
+end
+
 -- send the tweet
 local tw, err
-if img_file then
-    local media, err_ = client:upload_media{ media = assert(util.attach_file(img_file)) }
-    assert(media, tostring(err_))
-    media._request = nil  -- don't print binary data to the tty
+if media then
     print("media = " .. pretty.write(media))
     tw, err = media:tweet{ status = msg }
 else
@@ -35,7 +79,7 @@ else
 end
 
 -- the second return value contains the error if something went wrong, or http headers on success
-assert(tw, tostring(err))
+assert(tw, err)
 
 -- the result is json data in a Lua table
 print("user: @" .. tw.user.screen_name .. " (" .. tw.user.name .. ")")
